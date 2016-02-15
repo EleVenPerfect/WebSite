3be1eb2b45ca8a24e5e85aedfc6b8a43d3b1af93
ATIME <atime@atime.org.cn>
2016-02-15 22:31:12 +0800

    code

---
+++ ci/application/views/video.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+	<head>
+		<title>ATIME，专注创造奇迹！</title>
+		<?php $this->load->view('small/first') ?>
+	</head>
+	<body>
+		<?php $this->load->view('small/header') ?>
+		<IFRAME width=100% height=100% marginwidth=0 marginheight=0 SRC="http://i.youku.com/elevenperfect"  allowtransparency="yes" >
+			
+		</IFRAME>
+
+
+		<?php $this->load->view('small/copyright') ?>
+		<?php $this->load->view('small/last') ?>
+	</body>
+</html>
