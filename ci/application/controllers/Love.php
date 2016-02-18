3dd8fa36e09539aef33ca5bf1faf729032a4f47d
ATIME <atime@atime.org.cn>
2016-02-18 14:47:54 +0800

    Love

---
+++ ci/application/controllers/Love.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Love extends CI_Controller {
+
+	public function index()
+	{
+		// $this->load->view('welcome_message');
+		// $this->load->model('home_model','home');
+		// $this->output->cache(10);//一分钟缓存
+
+		// $data =$this->home->read_homepage();
+		// $data +=array(
+		// 	'home'  => $this->home->read_catagory(),
+		// );
+		// $this->output->enable_profiler(TRUE);
+		
+		$this->load->view('kk');
+	}
+
+}
