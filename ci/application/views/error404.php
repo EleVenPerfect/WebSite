5ee35b65bbcf61675e1cf710bb22a34a5a8b241a
ATIME <atime@atime.org.cn>
2016-12-08 00:22:08 +0800

    添加404页面

---
+++ ci/application/views/error404.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>404错误</title>
+<script type="text/javascript">setTimeout(function(){top.location='<?php echo site_url()?>';},6000)</script>
+<style type="text/css">
+body{text-align:center}
+h1{font-family:"微软雅黑"}
+</style>
+</head>
+
+<body>
+<p><img src="<?php echo base_url()?>bootstrap/images/error404.gif" width="520" height="320" /></p>
+<h1>抱歉，这个页面已经被外星人绑架了</h1>
+<p>5秒钟后将带您返回地球</p>
+</body>
+</html>
