078c60d7fa5c39e039689ea3565d10b243c15f5f
ATIME <atime@atime.org.cn>
2016-12-11 15:35:49 +0800

    细节修改

--- host/privacypolicy.php
+++ host/privacypolicy.php
@@ -27,13 +27,35 @@ include('geturl.php');
         <h2>Privacy Policy</h2>
 		<br/>
 	<div id="links">
-    The following discloses our information gathering and dissemination practices for this Web site.
 
-We use your IP address to help diagnose problems with our server, and to administer our Web site. Your IP address is used to help identify you and your ISP and to gather broad demographic information. We use cookies to save your password so you don't have to re-enter it each time you visit our site and for other purposes.
+<h3><p>We specifically do NOT allow the following types of sites</p>
+<p>服务器内严禁开设以下网站：</p>
+<p>Anything Illegal,违反法律</p>
+<p>Phishing，欺诈钓鱼</p>
+<p>Streaming，流媒体</p>
+<p>File Lockers，文件储藏</p>
+<p>Traffic Redirectors，网页重定向</p>
+<p>Forex / Banking sites，银行相关</p>
+<p>URL Shortening services，短地址服务</p>
+<p>Top Lists，排行</p>
+<p>Scripts that make automated FTP uploads，自动FTP上传</p>
+<p>Pharmaceutical，医药相关</p>
+<p>Viral，病毒相关</p>
+<p>Hacking，黑客</p>
+<p>Warez，盗版</p>
+<p>Proxy，代理</p>
+<p>Made for Adsense sites，Google Adsense</p>
+<p>Finance based sites (these include \'faucet\' sites)，经济相关</p>
+<p>Any Adult / Porn，成人网站</p>
+<p>Paid To Click，广告点击</p></h3>
 
-Our site uses an order form for customers to request information, products, and services. We collect visitor's contact information (like their email address), financial information (like their account or credit card numbers), and demographic information (like their zip code, age, or income level). Contact information from the order form is used to send information about our company and promotional material from some of our partners to our customers. The customer's contact information is also used to get in touch with the visitor when necessary and shared with other companies who may want to contact our visitors. Users may opt-out of receiving future mailings; see the choice/opt-out section below. Demographic and profile data is also collected at our site. We use this data to tailor our visitor's experience at our site, showing them content that we think they might be interested in, and displaying the content according to their preferences. This information is shared with advertisers on an aggregate basis.
+<p>    The following discloses our information gathering and dissemination practices for this Web site.</p>
 
-We use an outside ad company to display ads on our site. These ads may contain cookies. While we use cookies in other parts of our Web site, cookies received with banner ads are collected by our ad company, and we do not have access to this information. Some customer data is shared with the advertising companies.    
+<p>We use your IP address to help diagnose problems with our server, and to administer our Web site. Your IP address is used to help identify you and your ISP and to gather broad demographic information. We use cookies to save your password so you don't have to re-enter it each time you visit our site and for other purposes.</p>
+
+<p>Our site uses an order form for customers to request information, products, and services. We collect visitor's contact information (like their email address), financial information (like their account or credit card numbers), and demographic information (like their zip code, age, or income level). Contact information from the order form is used to send information about our company and promotional material from some of our partners to our customers. The customer's contact information is also used to get in touch with the visitor when necessary and shared with other companies who may want to contact our visitors. Users may opt-out of receiving future mailings; see the choice/opt-out section below. Demographic and profile data is also collected at our site. We use this data to tailor our visitor's experience at our site, showing them content that we think they might be interested in, and displaying the content according to their preferences. This information is shared with advertisers on an aggregate basis.</p>
+
+<p>We use an outside ad company to display ads on our site. These ads may contain cookies. While we use cookies in other parts of our Web site, cookies received with banner ads are collected by our ad company, and we do not have access to this information. Some customer data is shared with the advertising companies.</p>    
   </div>
     <div class="spacer"></div>
   </div>      
