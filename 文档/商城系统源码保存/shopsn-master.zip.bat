7932306b32e67e070983d8451955b11651655488
ATIME <atime@atime.org.cn>
2017-01-14 21:29:29 +0800

    源码保存

---
+++ 文档/商城系统源码保存/shopsn-master.zip.bat
@@ -0,0 +1,16 @@
+@echo off 
+echo -----------------------------------------------------
+echo -               欢迎使用软媒分割合并器              -
+echo -                                                   -
+echo -                                                   -
+echo ------------软媒官网：www.ruanmei.com----------------
+echo ------------软媒魔方官网：mofang.ruanmei.com---------
+echo -----------------------------------------------------
+echo 本批处理文件共分成 18 个子文件，共 86.23 M
+echo 请确认: 1、子文件的个数是否相符并放在当前目录下
+echo         2、可用剩余空间是否足够
+pause
+echo 开始合并文件, 合并过程中请勿关闭，合并时间长短与文件的大小有关
+echo 合并结束后会自动关闭当前窗口
+echo 请稍后... 
+copy /b shopsn-master.zip.rm1+shopsn-master.zip.rm2+shopsn-master.zip.rm3+shopsn-master.zip.rm4+shopsn-master.zip.rm5+shopsn-master.zip.rm6+shopsn-master.zip.rm7+shopsn-master.zip.rm8+shopsn-master.zip.rm9+shopsn-master.zip.rm10+shopsn-master.zip.rm11+shopsn-master.zip.rm12+shopsn-master.zip.rm13+shopsn-master.zip.rm14+shopsn-master.zip.rm15+shopsn-master.zip.rm16+shopsn-master.zip.rm17+shopsn-master.zip.rm18 shopsn-master.zip
