d75b6ecb427fc8a1964db08f9195cf0bdfe1d8d5
ATIME <atime@atime.org.cn>
2018-11-29 15:37:29 +0800

    更新

--- host/navigation.php
+++ host/navigation.php
@@ -4,9 +4,9 @@
           <li class="2"><a href="free-hosting.php" class="green">免费主机空间</a></li>
           <!-- <li class="3"><a href="premium-hosting.php" class="red">高级云主机</a></li> -->
           <!-- <li class="4"><a href="domains.php" class="blue">Domain Names</a></li> -->
-          <li class="4"><a href="whyus.php" class="purple">关于我们</a></li>
-          <li class="5"><a target="_blank" href="<? echo $supporturl;?>" class="grey">交流论坛</a></li>
-		      <li class="6"><a target="_blank" href="http://cpanel.<? echo "$yourdomain" ;?>" class="red">登录后台</a></li>
+          <li class="5"><a href="<? echo $supporturl;?>" class="grey">交流论坛</a></li>
+		  <li class="6"><a target="_blank" href="http://cpanel.<? echo "$yourdomain" ;?>" class="red">登录后台</a></li>
+		  <li class="4"><a href="whyus.php" class="purple">关于我们</a></li>
           <li class="1"><a href="en/index.php" class="yellow">For English</a></li>
         </ul>
       </div>
