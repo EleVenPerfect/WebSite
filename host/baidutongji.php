81ed0eaf868ee7aa688a57e2d0a45137c1fa32c2
ATIME <atime@atime.org.cn>
2019-01-20 21:03:00 +0800

    更新

--- host/baidutongji.php
+++ host/baidutongji.php
@@ -9,3 +9,13 @@
           enable_page_level_ads: true
      });
 </script>
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113731003-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-113731003-2');
+</script>
